466378b303c25326033b1d07ce8faf5ddec745be
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2024-06-08 20:33:26 -0400

    Read releases into memory and split on dividing lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let reader = BufReader::new(File::open("RELEASES.md").expect("file read"));
+
+    let lines = reader.lines().collect::<Result<Vec<_>, _>>().expect("should have valid lines");
+
+    let dividing_line_indices: Vec<_> = lines.iter().enumerate().filter_map(|(index, line)| {
+        (!line.is_empty() && line.chars().all(|c| c == '=')).then_some(index)
+    }).collect();
+
+    for window in dividing_line_indices.windows(2) {
+        let current_dividing_line = window[0];
+        let next_dividing_line = window[1];
+
+        let start_index = current_dividing_line - 1;
+        let end_index = next_dividing_line - 1;
+
+        for line in &lines[start_index..end_index] {
+            println!("{line}");
+        }
+    }
 }
