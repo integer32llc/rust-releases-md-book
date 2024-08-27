139f22a767f6e710184080891cef2c2824b5a23b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2024-08-27 10:56:38 -0400

    Save deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Meant to be run manually by someone with permissions to the repo. maybe someday in GH actions
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cargo run
+cd target/book
+mdbook build
+cd book
+
+git init
+git remote add upstream git@github.com:integer32llc/rust-releases-md-book.git
+git fetch upstream
+
+touch .
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push upstream HEAD:gh-pages
